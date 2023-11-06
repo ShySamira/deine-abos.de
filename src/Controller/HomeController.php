bb0b6557723624326e54be7bfa251ee14dbf49a5
Samira Sango <s.sango@onlineprinters.com>
2023-11-06 16:13:48 +0100

    first controller setup

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class HomeController
+{
+    public function index(Request $request):Response
+    {
+        $response = new Response();
+
+        $response->setContent("<p>HalloWelt</p>");
+
+        return new Response();
+    }
+}
